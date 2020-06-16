9fb9aad2b25cfe4203665a214777f6f8ce8887b4
Andrew Oliveira <48105321+DrewOliveira@users.noreply.github.com>
2020-06-15 23:32:58 -0300

    Commit

---
+++ Scripts/createTbCliente.sql
@@ -0,0 +1,9 @@
+use BDProjetoPOO
+Create table Cliente(
+	id integer primary key identity(1,1),
+	nome varchar(50),
+	sexo varchar(1),
+	datanasc datetime,
+	cpf varchar(11)
+)
+
---
+++ Scripts/createTbQuarto.sql
@@ -0,0 +1,8 @@
+use BDProjetoPOO
+create table Quarto(
+	id int primary key identity(1,1),
+	numero varchar(6),
+	tamanho int,
+	andar varchar(5),
+	tipo varchar(50)
+)
